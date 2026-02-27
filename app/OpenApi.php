c77cadc35af62216792a26b3725e1d9b6016a663
ertellucas98-dev <ertellucas98@gmail.com>
2026-02-27 17:11:49 -0300

    Adiciona classe OpenApi com anotações para documentação da API

---
+++ app/OpenApi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\OpenApi(
+ *   @OA\Info(
+ *     title="Laravel SaaS RBAC API",
+ *     version="1.0.0",
+ *     description="API documentation for the multi-tenant SaaS RBAC system"
+ *   )
+ * )
+ *
+ * @OA\PathItem(
+ *   path="/api"
+ * )
+ */
+class OpenApi
+{
+    // This class is only used for OpenAPI annotations.
+}
